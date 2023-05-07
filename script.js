8c3f7aae00173a942c762566f6487971810c3597
niangnia <1945219214@qq.com>
2023-05-07 18:10:07 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+  var response=prompt("What is your name?");
+  alert("Hello "+response+",may you a nice dream.");
+}
+
+function add(a,b){
+  return a+b;
+}
+console.log(add(20,30));
+console.log(add(200,300));
+
+
+function sayHello(){
+  var x =document.getElementById("demo");
+  x.style.fontSize="25px";
+  x.style.color="red";
+  x.innerHTML = "Well,nightynight^^";
+}
