963eaaf873f081ac8a028f3da9c454e41a0e28c0
niangnia <1945219214@qq.com>
2023-05-07 18:10:10 +0800

    Create script1.js

---
+++ script1.js
@@ -0,0 +1,32 @@
+function back(){
+  window.open("index.html");
+  alert(window.location);
+}
+
+
+function coreCount(){
+	var cores=0;
+	var qestions=document.getElementsByClassName("question")
+	for(var i=0;i<qestions.length;i++)
+	{
+		var count=0;
+		var flag=1;
+		var answer=document.getElementsByName("answer"+(i+1));
+		for(var j=0;j<answer.length;j++)
+		{
+			if(answer[j].checked)
+			{
+				if(answer[j].value*1<0)
+				{
+					flag=0;
+					break;
+				}
+				else
+					count+=answer[j].value*1;
+			}
+		}
+		if(flag==1)
+			cores+=count;
+	}
+	alert("Your score is "+cores);
+}
